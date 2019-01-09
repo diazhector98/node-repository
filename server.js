f3c95e4c674636014279b51097bf87361a86f430
Hector Diaz <diazhector98@gmai.com>
2019-01-09 16:19:30 -0600

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -50,4 +51,6 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Not found'
     })
 });
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up in ${port}`);
+});
